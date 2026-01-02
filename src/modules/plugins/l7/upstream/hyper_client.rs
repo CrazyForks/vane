7f58c92b636a6b69f52be789a8955750a7c6f912
Canmi <9997200@qq.com>
2026-01-02 23:42:18 +0800

    refactor: update Hyper client to new PayloadState with BufferGuard

--- src/modules/plugins/l7/upstream/hyper_client.rs
+++ src/modules/plugins/l7/upstream/hyper_client.rs
@@ -44,7 +44,7 @@ pub async fn execute_hyper_request(
 
 	let body: BoxBody<Bytes, Error> = match req_payload {
 		PayloadState::Http(vane_body) => vane_body.boxed(),
-		PayloadState::Buffered(bytes) => Full::new(bytes).map_err(|e| match e {}).boxed(),
+		PayloadState::Buffered(bytes, _guard) => Full::new(bytes).map_err(|e| match e {}).boxed(),
 		PayloadState::Empty | PayloadState::Generic => BoxBody::default(),
 	};
 
@@ -127,7 +127,7 @@ pub async fn execute_h1_websocket_request(
 
 	let body: BoxBody<Bytes, Error> = match req_payload {
 		PayloadState::Http(vane_body) => vane_body.boxed(),
-		PayloadState::Buffered(bytes) => Full::new(bytes).map_err(|e| match e {}).boxed(),
+		PayloadState::Buffered(bytes, _guard) => Full::new(bytes).map_err(|e| match e {}).boxed(),
 		PayloadState::Empty | PayloadState::Generic => BoxBody::default(),
 	};
 
