4222fd90491ec77b597051f0e3460a5daf382cce
Canmi <9997200@qq.com>
2026-01-03 17:51:11 +0800

    refactor: update import path for ip module

--- src/modules/stack/transport/proxy/udp.rs
+++ src/modules/stack/transport/proxy/udp.rs
@@ -1,6 +1,6 @@
 /* src/modules/stack/transport/proxy/udp.rs */
 
-use crate::common::{config::getenv, ip};
+use crate::common::{config::getenv, net::ip};
 use crate::modules::stack::transport::{
 	health,
 	model::ResolvedTarget,
