7f34a8a12ab8a75e3a61293f20f02b7631ba5766
Canmi <9997200@qq.com>
2026-01-03 17:34:05 +0800

    refactor: Update `bootstrap.rs` import paths for common modules.

--- src/core/bootstrap.rs
+++ src/core/bootstrap.rs
@@ -5,7 +5,7 @@ use fancy_log::{LogLevel, log};
 use std::sync::Arc;
 use tokio::signal;
 
-use crate::common::{getenv, lifecycle, watcher};
+use crate::common::{config::getenv, lifecycle, watcher};
 use crate::core::{console, logging, monitor};
 use crate::modules::{
 	certs, nodes,
