ce764484fbbf02d17e91a5971b5dd6db034fac37
Canmi <9997200@qq.com>
2026-01-03 19:48:01 +0800

    refactor: update KvStore import path to resources module

--- src/modules/flow/context.rs
+++ src/modules/flow/context.rs
@@ -7,9 +7,9 @@ use std::collections::HashMap;
 use async_trait::async_trait;
 use serde_json::Value;
 
-use crate::modules::kv::KvStore;
 use crate::modules::stack::application::container::Container;
 use crate::modules::template::{context::L7Context, context::SimpleContext, resolve_inputs};
+use crate::resources::kv::KvStore;
 
 /// Execution context abstraction for flow engine.
 ///
