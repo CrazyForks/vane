080a0a06dfb14e4c8dc6fe5bb9fbc84fd5f340db
Canmi <9997200@qq.com>
2026-01-03 20:01:00 +0800

    refactor: update certs module import in bootstrap

--- src/core/bootstrap.rs
+++ src/core/bootstrap.rs
@@ -11,7 +11,7 @@ use crate::common::{
 };
 use crate::core::{console, logging, monitor};
 use crate::modules::{
-	certs, nodes,
+	nodes,
 	plugins::core::loader as plugin_loader,
 	ports,
 	stack::{
@@ -19,6 +19,7 @@ use crate::modules::{
 		carrier::{hotswap as resolver_hotswap, model as resolver_model},
 	},
 };
+use crate::resources::certs;
 
 /// Entry point for the Vane bootstrap sequence.
 pub async fn start() {
