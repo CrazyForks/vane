06e0b7669d99604345204f5f27d4abf431d100c7
Canmi <9997200@qq.com>
2026-01-03 15:26:40 +0800

    perf: Use AHashMap for initial payloads in carrier flow

--- src/modules/stack/carrier/flow.rs
+++ src/modules/stack/carrier/flow.rs
@@ -9,14 +9,13 @@ use crate::modules::{
 };
 
 use bytes::Bytes;
-use std::collections::HashMap;
 
 pub async fn execute(
 	step: &ProcessingStep,
 	kv: &mut KvStore,
 	conn: ConnectionObject,
 	parent_path: String,
-	initial_payloads: HashMap<String, Bytes>,
+	initial_payloads: ahash::AHashMap<String, Bytes>,
 ) -> Result<TerminatorResult> {
 	kv.insert("conn.layer".to_string(), "l4p".to_string());
 
